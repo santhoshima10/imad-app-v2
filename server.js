6018c32d6311accd8637d8e7a44b24e6bb699cca
santhoshima10 <santhoshima.work@gmail.com>
2017-02-15 15:17:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,6 +21,14 @@ app.get('/article-one',function(req,res){
   res.send('Hello Article One');  
 });
 
+app.get('/article-two',function(req,res){
+   res.send('Hello Article two');   
+});
+
+app.get('/article-Three',function(req,res){
+   res.send('Hello Article Three');   
+});
+
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
