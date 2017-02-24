44885604ee57a81804f7f2be6dd0d4fffc6f862d
santhoshima10 <santhoshima.work@gmail.com>
2017-02-24 21:26:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -80,6 +80,10 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
 app.get('/:articleName',function(req,res){
     var articleName = req.params.articleName;
    res.send(createHtmlTemplate(articles[articleName]))
