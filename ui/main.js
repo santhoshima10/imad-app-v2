36e1b2b03aaa1f5be9c37eaa498cbd7ad67a305b
santhoshima10 <santhoshima.work@gmail.com>
2017-02-25 15:03:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,15 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'Hello World';
 
 var img = document.getElementById('madi');
+
+var margin = 0;
+
+function moveRight(){
+    margin = margin + 10;
+    img.style.marginRight = margin+'px';
+    
+}
+
 img.onclick = function(){
-    img.style.marginLeft = '100px';
+    var interval = setInterval(moveRight, 50);
 };
