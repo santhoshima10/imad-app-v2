c398cf567d388a031ec619c542f725b0395d26a8
santhoshima10 <santhoshima.work@gmail.com>
2017-02-28 12:04:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,16 +3,16 @@ var button = document.getElementById('counter');
 button.onclick=function(){
       alert('Inside the onclick') ;  
     
-var request = xmlHttpRequest();
+var request = XMLHttpRequest();
 request.open('GET','http://santhoshima10.imad.hasura-app.io/counter',true);
 request.send(null);
 
 request.onreadystatechange = function(){
     
     if(request.readyState === XMLHttpRequest.DONE){
-        alert('Inside the 1') ; 
+        //alert('Inside the 1') ; 
          if(request.status === 200){
-          alert('Inside the 2') ;  
+         // alert('Inside the 2') ;  
         var count = document.getElementById('count');
         count.innerHTML = request.responseText.toString();
         
