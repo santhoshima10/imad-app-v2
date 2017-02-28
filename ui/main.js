89b8cee3be730b2c17619f471af0f1a80c1ea9e7
santhoshima10 <santhoshima.work@gmail.com>
2017-02-28 11:16:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,2 @@
-console.log('Loaded!');
-
-var img = document.getElementById('madi');
-
-var margin = 0;
-
-function moveRight(){
-    margin = margin + 10;
-    img.style.marginRight = margin+'px';
-    
-}
-
-img.onclick = function(){
-    var interval = setInterval(moveRight, 50);
-};
+var count = document.getElementById('count');
+count.innerHTML = 12;
