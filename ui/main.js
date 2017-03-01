d2e6be55bbe3dd4f999823e4b0f926b896527474
santhoshima10 <santhoshima.work@gmail.com>
2017-03-01 14:53:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -32,36 +32,38 @@ var submitBtn = document.getElementById('submit_btn');
 
 submitBtn.onclick=function(){
     
-        var request = new XMLHttpRequest();
-request.open('GET','http://santhoshima10.imad.hasura-app.io/counter',true);
+var nameInput = document.getElementById('nameInput');
+var nameInputValue = nameInput.value;
+
+    
+var request = new XMLHttpRequest();
+request.open('GET','http://santhoshima10.imad.hasura-app.io/submit?name='+nameInputValue,true);
 request.send(null);
 
 request.onreadystatechange = function(){
     
     if(request.readyState === XMLHttpRequest.DONE){
-        //alert('Inside the 1') ; 
+      
          if(request.status === 200){
-         // alert('Inside the 2') ;  
-        var count = document.getElementById('count');
-        count.innerHTML = request.responseText.toString();
-        
-        
+             uList = document.getElementById('NameList');
+             var output = request.responseText ;
+             var names = JSON.parse(output);
+             
+
+            for (var i = 0 ;i < names.length;i++){
+                uList.innerHTML += '<li>'+names[i]+'</li>';
+            }
+             
+         
+       
     }
         
     }
     
 }
-
-var nameInput = document.getElementById('nameInput');
-var nameInputValue = nameInput.value;
-
-var uList = document.getElementById('NameList');
-
-var names = ["name1","name2","name3"];
-
-for (var i = 0 ;i < names.length;i++){
-    uList.innerHTML += '<li>'+names[i]+'</li>';
 }
+
+
 
 
 
